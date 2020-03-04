c100bf9221d385b76baff13f58151abcb63a9976
rishibodake <rishibodake@gmail.com>
2020-03-04 16:32:51 +0530

    Added:WagecalculatingFeature

--- employeeWageCalculator.sh
+++ employeeWageCalculator.sh
@@ -2,14 +2,22 @@
 #Discription:calculate the wages of employee using use cases
 #Author:Hrishikesh M Bodake
 #Since:March 4 2020 /Wednesday
+isPresent=1
+wagePerHour=20
+fullTimeHour=8
 
 function attendence(){
 	randomGenrator=$((RANDOM%2))
-	if [[ $randomGenrator -eq 1 ]]
+	if [[ $randomGenrator -eq $isPresent ]]
 	then
 		echo Employee is Present
+		dailywage #if present then calculate wages
 	else
 		echo Employee is Absent
 	fi
 }
-attendence
+function dailyWage(){
+	dailySalary=$(($wagePerHour*$fullTimeHour))
+	echo $dailySalary
+}
+dailyWage
