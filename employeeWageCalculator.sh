1c6a92d09a09baebeaffb9c577fa45486e2187d3
rishibodake <rishibodake@gmail.com>
2020-03-04 16:50:49 +0530

    Added:PartTimeCheckerFeature

--- employeeWageCalculator.sh
+++ employeeWageCalculator.sh
@@ -3,21 +3,36 @@
 #Author:Hrishikesh M Bodake
 #Since:March 4 2020 /Wednesday
 isPresent=1
+ispartTime=1
 wagePerHour=20
+workHours=0
 fullTimeHour=8
+partTimeHour=4
 
 function attendence(){
 	randomGenrator=$((RANDOM%2))
 	if [[ $randomGenrator -eq $isPresent ]]
 	then
 		echo Employee is Present
-		dailywage #if present then calculate wages
+		checkPartTime
 	else
 		echo Employee is Absent
 	fi
 }
 function dailyWage(){
-	dailySalary=$(($wagePerHour*$fullTimeHour))
+	dailySalary=$(($wagePerHour*$workHours))
 	echo $dailySalary
 }
-dailyWage
+function checkPartTime(){
+	if [[ $((RANDOM%2)) -eq $ispartTime ]]
+	then
+		echo Employee is PartTime
+		workHours=$partTimeHour
+		dailyWage
+	else
+		echo Employee is FullTime
+		workHours=$fullTimeHour
+		dailyWage
+	fi
+}
+attendence
